87eb54bb2fb8cfec8c1b1063eca9476f8effb11b
Luis Guisado <luis.guisado.matrix@gmail.com>
2023-10-12 01:11:17 -0500

    feat: connect postgresql with nestjs

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
-    ConfigModule.forRoot()
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      database: process.env.DB_NAME,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      autoLoadEntities: true,
+      synchronize: true // Solo para DEV, no suele usar en PROD.
+    })
   ],
 })
 export class AppModule { }
