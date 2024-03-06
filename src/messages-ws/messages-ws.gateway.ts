04eeb2539f9d92eaef15f896d3398d963da082b3
Luis Guisado <lguisadom@gmail.com>
2024-03-05 20:46:15 -0500

    feat: Websocket Gateways

--- src/messages-ws/messages-ws.gateway.ts
+++ src/messages-ws/messages-ws.gateway.ts
@@ -1,7 +1,15 @@
-import { WebSocketGateway } from '@nestjs/websockets';
+import { OnGatewayConnection, OnGatewayDisconnect, WebSocketGateway } from '@nestjs/websockets';
 import { MessagesWsService } from './messages-ws.service';
 
 @WebSocketGateway({ cors: true })
-export class MessagesWsGateway {
-  constructor(private readonly messagesWsService: MessagesWsService) {}
+export class MessagesWsGateway implements OnGatewayConnection, OnGatewayDisconnect {
+
+  constructor(private readonly messagesWsService: MessagesWsService) { }
+  
+  handleConnection(client: any, ...args: any[]) {
+    throw new Error('Method not implemented.');
+  }
+  handleDisconnect(client: any) {
+    throw new Error('Method not implemented.');
+  }
 }
