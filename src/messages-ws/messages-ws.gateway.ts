137c1d640fc5924c24bfc39c1f28498f62e46e2c
luisguisadomatrix <lguisadom@gmail.com>
2024-04-17 00:51:33 -0500

    udpate

--- src/messages-ws/messages-ws.gateway.ts
+++ src/messages-ws/messages-ws.gateway.ts
@@ -1,23 +1,52 @@
-import { OnGatewayConnection, OnGatewayDisconnect, WebSocketGateway } from '@nestjs/websockets';
+import { OnGatewayConnection, OnGatewayDisconnect, SubscribeMessage, WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
 import { MessagesWsService } from './messages-ws.service';
-import { Socket } from 'socket.io';
+import { Server, Socket } from 'socket.io';
+import { NewMessageDto } from './dtos/new-message.dto';
 
 @WebSocketGateway({ cors: true })
 export class MessagesWsGateway implements OnGatewayConnection, OnGatewayDisconnect {
+
+  @WebSocketServer() wss: Server;
 
   constructor(
     private readonly messagesWsService: MessagesWsService
   ) { }
 
   handleConnection(client: Socket) {
-    //console.log('Client connected: ', client.id);
+    const token: string = client.handshake.headers.authentication as string;
+    console.log({ token });
     this.messagesWsService.registerClient(client);
-    console.log({ clients: this.messagesWsService.getConnectedClients() });
+
+    this.wss.emit('clients-updated', this.messagesWsService.getConnectedClients());
   }
 
   handleDisconnect(client: Socket) {
     // console.log('Client disconnected: ', client.id);
     this.messagesWsService.removeClient(client.id);
-    console.log({ clients: this.messagesWsService.getConnectedClients() });
+
+    this.wss.emit('clients-updated', this.messagesWsService.getConnectedClients());
+  }
+
+  @SubscribeMessage('message-from-client')
+  handleMessageFromClient(client: Socket, payload: NewMessageDto) {
+    console.log(client.id, payload);
+
+    // Send message to the same client
+    // client.emit('message-from-server', { 
+    //   fullName: 'Soy yo!', 
+    //   message: payload.message || 'no-message!!'
+    // });
+
+    // Send message to everyone except the client who sent the message
+    // client.broadcast.emit('message-from-server', {
+    //   fullName: 'Soy yo!', 
+    //   message: payload.message || 'no-message!!'
+    // });
+
+    // Send message to everyone
+    this.wss.emit('message-from-server', {
+      fullName: 'Soy yo!',
+      message: payload.message || 'no-message!!'
+    });
   }
 }
